564359a12096ebcf718e1b3d0b2f9e509790ecfa
Leonardo Maldonado <lmaldonado@crm.local>
2026-05-03 17:25:10 -0300

    22

--- Mundo-Pet-PIM--main/app.js
+++ Mundo-Pet-PIM--main/app.js
@@ -1520,7 +1520,30 @@
                 </div>
                 <div>
                     <label class="text-xs font-bold text-gray-500 mb-1 block">Hora *</label>
-                    <input id="agenda-hora" type="time" class="input-pet" required>
+                    <select id="agenda-hora" class="input-pet" required>
+                        <option value="">Selecione...</option>
+                        <option value="08:00">08:00</option>
+                        <option value="08:30">08:30</option>
+                        <option value="09:00">09:00</option>
+                        <option value="09:30">09:30</option>
+                        <option value="10:00">10:00</option>
+                        <option value="10:30">10:30</option>
+                        <option value="11:00">11:00</option>
+                        <option value="11:30">11:30</option>
+                        <option value="12:00">12:00</option>
+                        <option value="12:30">12:30</option>
+                        <option value="13:00">13:00</option>
+                        <option value="13:30">13:30</option>
+                        <option value="14:00">14:00</option>
+                        <option value="14:30">14:30</option>
+                        <option value="15:00">15:00</option>
+                        <option value="15:30">15:30</option>
+                        <option value="16:00">16:00</option>
+                        <option value="16:30">16:30</option>
+                        <option value="17:00">17:00</option>
+                        <option value="17:30">17:30</option>
+                        <option value="18:00">18:00</option>
+                    </select>
                 </div>
             </div>
             <div>
